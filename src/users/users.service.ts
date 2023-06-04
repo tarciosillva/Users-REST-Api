0cbb6fb1a133e21a39ade8f4ffd7fda8a0d8f81d
Tarcio Sillva <tarciosillva7@gmail.com>
2023-06-04 00:26:52 -0300

    service update

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,82 +2,52 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Connection } from 'mongoose';
 import { User } from './schemas/user.schema';
+import { phoneValidation } from './utils/phoneValidation';
+import { cpfValidation } from './utils/cpfValidation';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(User.name) private readonly userModel: Model<User>) { }
+  constructor(@InjectModel(User.name) private readonly connection: Connection) { }
+
   async create(createUserDto: CreateUserDto) {
-    const regexPhone = /^(?:\((?=\d{2}\))(\d{2})\)\s?)?(9\d{8})$/;
-    if (!regexPhone.test(createUserDto.telefone)) {
+    if (!phoneValidation(createUserDto.telefone)) {
       return {
         success: false,
         msg: 'Telefone inválido.'
       }
+
     }
-    if (!this.cpfValidation(createUserDto.cpf)) {
+    if (!cpfValidation(createUserDto.cpf)) {
       return {
         success: false,
         msg: 'CPF inválido.'
       }
     }
-
-    await this.userModel.create(createUserDto);
-
+    await this.connection.model(User.name).create(createUserDto);
     return {
       success: true
     };
-
-  }
-
-  cpfValidation(cpf: string) {
-    cpf = cpf.replace(/[^\d]+/g, '');
-    if (cpf == '') return false;
-    if (cpf.length != 11 ||
-      cpf == "00000000000" ||
-      cpf == "11111111111" ||
-      cpf == "22222222222" ||
-      cpf == "33333333333" ||
-      cpf == "44444444444" ||
-      cpf == "55555555555" ||
-      cpf == "66666666666" ||
-      cpf == "77777777777" ||
-      cpf == "88888888888" ||
-      cpf == "99999999999")
-      return false;
-    var add = 0;
-    for (var i = 0; i < 9; i++)
-      add += parseInt(cpf.charAt(i)) * (10 - i);
-    var rev = 11 - (add % 11);
-    if (rev == 10 || rev == 11)
-      rev = 0;
-    if (rev != parseInt(cpf.charAt(9)))
-      return false;
-    add = 0;
-    for (i = 0; i < 10; i++)
-      add += parseInt(cpf.charAt(i)) * (11 - i);
-    rev = 11 - (add % 11);
-    if (rev == 10 || rev == 11)
-      rev = 0;
-    if (rev != parseInt(cpf.charAt(10)))
-      return false;
-    return true;
   }
 
   async findAll() {
-    const result = await this.userModel.find().exec()
+    const result = await this.connection.model(User.name).find()
     if (!result) {
       return {
         success: false,
         msg: 'Não há usuários cadastrados'
       }
     }
-    return result
+
+    return {
+      result,
+      success: true
+    }
   }
 
   async findOne(cpf: string) {
-    const result = await this.userModel.findOne({ cpf: cpf }).exec()
+    const result = await this.connection.model(User.name).findOne({ cpf: cpf })
     if (!result) {
       return {
         success: false,
@@ -85,11 +55,14 @@ export class UsersService {
       }
     }
 
-    return result
+    return {
+      result,
+      success: true
+    }
   }
 
   async update(id: string, updateUserDto: UpdateUserDto) {
-    const result = await this.userModel.findByIdAndUpdate({ _id: id }, updateUserDto)
+    const result = await this.connection.model(User.name).findByIdAndUpdate({ _id: id }, updateUserDto)
     if (!result) {
       return {
         success: false,
@@ -98,16 +71,17 @@ export class UsersService {
     }
 
     return {
+      result,
       success: true,
     }
   }
 
   async remove(id: string) {
-    const result = await this.userModel.deleteOne({ _id: id })
+    const result = await this.connection.model(User.name).findOneAndDelete({ _id: id })
     if (!result) {
       return {
         success: false,
-        msg: 'Usuário inválido'
+        msg: 'Usuário inválido.'
       }
     }
 
