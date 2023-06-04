50290f96333644fd3ab2e8ef453867e516392cc4
Tarcio Sillva <tarciosillva7@gmail.com>
2023-06-04 16:29:27 -0300

    events module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,14 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MongooseConfigService } from './database/mongoose-config';
-
+import { EventsModule } from './events/events.module';
 @Module({
   imports: [
     UsersModule,
     MongooseModule.forRootAsync({
       useClass: MongooseConfigService,
     }),
+    EventsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
