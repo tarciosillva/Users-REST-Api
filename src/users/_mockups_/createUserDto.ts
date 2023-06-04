dd0de78242a9b118d521d8140aaed03b8bf856eb
Tarcio Sillva <tarciosillva7@gmail.com>
2023-06-04 00:27:22 -0300

    create mock user dto

---
+++ src/users/_mockups_/createUserDto.ts
@@ -0,0 +1,8 @@
+import { CreateUserDto } from "../dto/create-user.dto";
+
+export const createUserDto: CreateUserDto = {
+    nome: 'Tarcio',
+    sobrenome: 'Sousa',
+    cpf: '04157000277',
+    telefone: '(94)984097406'
+}
