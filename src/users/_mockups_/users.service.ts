542491967849a28b2d308d40928b34f3834e28bc
Tarcio Sillva <tarciosillva7@gmail.com>
2023-06-04 00:28:33 -0300

    create mock user.service

--- src/users/_mockups_/users.service.ts
+++ src/users/_mockups_/users.service.ts
@@ -1,11 +1,98 @@
+import { CreateUserDto } from "../dto/create-user.dto"
+import { UpdateUserDto } from "../dto/update-user.dto"
+import { UserInterface } from "../interface/user-interface"
 import { userStub } from "../stubs/user.stub"
+import { cpfValidation } from "../utils/cpfValidation"
+import { phoneValidation } from "../utils/phoneValidation"
 
-export const UsersService = jest.fn().mockReturnValue({
-    create: jest.fn().mockResolvedValue(userStub),
-    phoneValidation: jest.fn().mockResolvedValue([userStub]),
-    cpfValidation: jest.fn().mockResolvedValue(userStub),
-    findAll: jest.fn().mockResolvedValue(userStub),
-    findOne: jest.fn().mockResolvedValue(userStub),
-    update: jest.fn().mockResolvedValue(userStub),
-    remove: jest.fn().mockResolvedValue(userStub),
-})
+export class UsersServiceMock {
+    async create(createUserDto: CreateUserDto) {
+        if (!phoneValidation(createUserDto.telefone)) {
+            return {
+                success: false,
+                msg: 'Telefone inválido.'
+            }
+
+        }
+        if (!cpfValidation(createUserDto.cpf)) {
+            return {
+                success: false,
+                msg: 'CPF inválido.'
+            }
+        }
+
+        return {
+            success: true
+        };
+    }
+
+    async findAll() {
+        const result = [userStub, userStub]
+        if (!result) {
+            return {
+                success: false,
+                msg: 'Não há usuários cadastrados'
+            }
+        }
+
+        return {
+            result,
+            success: true
+        }
+    }
+
+    async findOne(cpf: string) {
+        const result = [userStub].find((user => user.cpf === cpf))
+        if (!result) {
+            return {
+                success: false,
+                msg: 'Informações de CPF não armazenadas.'
+            }
+        }
+
+        return {
+            result,
+            success: true
+        }
+    }
+    
+    async update(id: string, updateUserDto: UpdateUserDto) {
+        var user = [userStub].find((user => user._id === id))
+        var result: UserInterface
+        if (user) {
+            result = {
+                _id: user._id,
+                cpf: updateUserDto.cpf,
+                nome: updateUserDto.nome,
+                sobrenome: updateUserDto.sobrenome,
+                telefone: updateUserDto.telefone
+            }
+        }
+
+        if (!result) {
+            return {
+                success: false,
+                msg: 'Usuário inválido'
+            }
+        }
+
+        return {
+            result,
+            success: true,
+        }
+    }
+    async remove(id: string) {
+        const result = [userStub].find((user => user._id === id))
+        if (!result) {
+            /* Usuário não foi encontrado */
+            return {
+                success: false,
+                msg: 'Usuário inválido.'
+            }
+        }
+
+        return {
+            success: true
+        }
+    }
+}
