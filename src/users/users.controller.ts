96aa1ddb23698500a6bf66ed798448112e553c1f
Tarcio Sillva <tarciosillva7@gmail.com>
2023-06-05 08:40:03 -0300

    Add @Roles, @Public routes controllers

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,31 +2,39 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, Put } from '@nestjs/
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Public } from '../auth/decorator/public.decorator';
+import { Roles } from '../auth/decorator/roles.decorator';
+import { Role } from '../auth/role.enum';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) { }
 
+  @Roles(Role.Admin)
   @Post()
   async create(@Body() createUserDto: CreateUserDto) {
     return await this.usersService.create(createUserDto);
   }
 
+  @Public()
   @Get()
   async findAll() {
     return await this.usersService.findAll();
   }
 
+  @Roles(Role.Admin)
   @Get(':cpf')
   async findOne(@Param('cpf') cpf: string) {
     return await this.usersService.findOne(cpf);
   }
 
+  @Roles(Role.Admin)
   @Put(':id')
   async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return await this.usersService.update(id, updateUserDto);
   }
 
+  @Roles(Role.Admin)
   @Delete(':id')
   async remove(@Param('id') id: string) {
     return await this.usersService.remove(id);
