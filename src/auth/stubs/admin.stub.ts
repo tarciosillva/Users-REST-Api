9eaf005e2ec0aaf1169f2cefba28dd4acd8c4924
Tarcio Sillva <tarciosillva7@gmail.com>
2023-06-05 08:24:26 -0300

    create admin.stub.ts

---
+++ src/auth/stubs/admin.stub.ts
@@ -0,0 +1,11 @@
+import { AdminInterface } from "../interface/adminInterface"
+
+export const adminStub: AdminInterface = {
+    _id:"123",
+    nome: "Bluelab",
+    sobrenome: "services",
+    telefone: "(94)000000000",
+    token:'sdfasdf454ds654sd654fsAFJA8d5A',
+    password:"1234",
+    role:"Admin"
+}
