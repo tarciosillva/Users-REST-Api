506341372addd60988bd6ea5f411d73866087ea6
Tarcio Sillva <tarciosillva7@gmail.com>
2023-06-05 08:37:18 -0300

    Import AdminScheme, AdminUser, add AuthGuard in provider

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,22 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schema';
-
+import { AdminUser, AdminUserSchema } from "./schemas/admin.schema"
+import { AuthGuard } from '../auth/auth.guard';
+import { APP_GUARD } from '@nestjs/core';
 @Module({
-  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
+  imports: [MongooseModule.forFeature([
+    { name: User.name, schema: UserSchema },
+    { name: AdminUser.name, schema: AdminUserSchema }
+  ])],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [
+    UsersService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
+  exports:[UsersService]
 })
-export class UsersModule {}
+export class UsersModule { }
