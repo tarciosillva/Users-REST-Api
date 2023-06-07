bc9b1413e066757877c353016f6bc9a47af74c5c
Tarcio Sillva <tarciosillva7@gmail.com>
2023-06-07 13:17:07 -0300

    websocket gatway

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -2,7 +2,16 @@ import { Logger } from '@nestjs/common';
 import { OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway(80)
+interface UserSocket {
+  nome: string,
+  id: string
+}
+@WebSocketGateway(80, {
+  cors: {
+    origin: '*',
+  }
+})
+
 export class EventsGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer() server: Server;
   private logger: Logger = new Logger('EventsGateway');
@@ -12,16 +21,24 @@ export class EventsGateway implements OnGatewayInit, OnGatewayConnection, OnGate
     this.server.emit('msgToClient', payload, client.id);
   }
 
+  @SubscribeMessage('privateMessage')
+  handlePrivateMessage(client: Socket, payload: { recipient: string, message: string }): void {
+    const { recipient, message } = payload;
+    client.emit('privateMessage', message);
+    this.server.to(recipient).emit('privateMessage', message);
+  }
+
   afterInit(server: Server) {
     this.logger.log('Init');
   }
 
-  handleConnection(client: Socket) {
-    console.log(client.id)
-    this.logger.log( `Client connected: ${client.id}`);
+  @SubscribeMessage('join')
+  handleConnection(client: Socket): void {
+    client.emit('welcome', `Bem-vindo, usuário ${client.id}!`);
   }
 
+  @SubscribeMessage('disconnectTosocket')
   handleDisconnect(client: Socket) {
-    this.logger.log( `Client disconnected: ${client.id}`);
+    client.disconnect();
   }
 }
