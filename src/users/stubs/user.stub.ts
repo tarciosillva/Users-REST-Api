7c0fa46be1044630590693f42ebae34080510fd4
Tarcio Sillva <tarciosillva7@gmail.com>
2023-06-07 13:23:18 -0300

    edit stub

--- src/users/stubs/user.stub.ts
+++ src/users/stubs/user.stub.ts
@@ -2,8 +2,8 @@ import { UserInterface } from "../interface/user.interface"
 
 export const userStub: UserInterface = {
     _id: '123',
-    cpf: '04157000277',
+    cpf: '05865444833',
     nome: 'Tarcio',
     sobrenome: 'Sousa',
-    telefone: '(94)984097406'
+    telefone: '(94)984125566'
 }
